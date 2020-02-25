619813e68563596e225482a4d631978f8f65ff2b
Sailoog <info@sailoog.com>
2020-02-25 19:35:51 +0100

    use can postInstall

--- stage6/04-install-can/01-run.sh
+++ stage6/04-install-can/01-run.sh
@@ -1,17 +1,5 @@
 #!/bin/bash -e
 
-install -m 644 files/openplotter-can-read.service	"${ROOTFS_DIR}/etc/systemd/system/"
 on_chroot << EOF
-echo "ExecStart=openplotter-can-read" >> "/etc/systemd/system/openplotter-can-read.service"
-echo "StandardOutput=syslog" >> "/etc/systemd/system/openplotter-can-read.service"
-echo "StandardError=syslog" >> "/etc/systemd/system/openplotter-can-read.service"
-echo "WorkingDirectory=/home/${FIRST_USER_NAME}" >> "/etc/systemd/system/openplotter-can-read.service"
-echo "User=root" >> "/etc/systemd/system/openplotter-can-read.service"
-echo "[Install]" >> "/etc/systemd/system/openplotter-can-read.service"
-echo "WantedBy=multi-user.target" >> "/etc/systemd/system/openplotter-can-read.service"
-EOF
-
-on_chroot << EOF
-systemctl daemon-reload
-systemctl enable openplotter-can-read.service
+sudo -u ${FIRST_USER_NAME} sudo canPostInstall
 EOF
