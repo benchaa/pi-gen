5c3c1544bfb07c09f66bb358e15a92bcfba0fd19
Sailoog <info@sailoog.com>
2020-02-26 20:27:35 +0100

    update moitessier post install

--- stage6/12-set-moitessier/01-run.sh
+++ stage6/12-set-moitessier/01-run.sh
@@ -1,24 +1,31 @@
 #!/bin/bash -e
 
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.openplotter/moitessier"
-install -m 644 -o 1000 -g 1000 files/moitessier_4.19.75_1.4.0_armhf.deb		"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.openplotter/moitessier/"
+on_chroot << EOF
+sudo -u ${FIRST_USER_NAME} sudo moitessierPostInstall
+EOF
 
 install -m 644 files/10-openplotter.rules	"${ROOTFS_DIR}/etc/udev/rules.d/"
 install -m 644 -o 1000 -g 1000 files/settings.json		"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.signalk/"
 install -m 644 -o 1000 -g 1000 files/sk-to-nmea0183.json		"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.signalk/plugin-config-data/"
-install -m 644 -o 1000 -g 1000 files/openplotter.conf		"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.openplotter/"
 install -m 644 -o 1000 -g 1000 files/opencpn.conf		"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.opencpn/"
 
 on_chroot << EOF
+echo "[UDEV]" >> "/home/${FIRST_USER_NAME}/.openplotter/openplotter.conf"
+echo "serialinst = {'ttyOP_hat': {'device': 'moitessier.tty', 'vendor': '', 'product': '', 'port': 'virtual', 'serial': '', 'remember': 'port', 'data': 'NMEA 0183'}}" >> "/home/${FIRST_USER_NAME}/.openplotter/openplotter.conf"
+echo "[I2C]" >> "/home/${FIRST_USER_NAME}/.openplotter/openplotter.conf"
+echo "sensors = {'MS5607-02BA03': {'address': '0x77', 'port': 51000, 'data': [{'SKkey': 'environment.outside.pressure', 'rate': 5.0, 'offset': 0.0}, {'SKkey': 'environment.inside.openplotter.temperature', 'rate': 5.0, 'offset': 0.0}]}}" >> "/home/${FIRST_USER_NAME}/.openplotter/openplotter.conf"
+
 echo "i2c-dev" >> "/etc/modules"
 sed -i 's/#dtparam=i2c_arm=on/dtparam=i2c_arm=on/g' /boot/config.txt
-
 sed -i 's/#dtparam=spi=on/dtparam=spi=on/g' /boot/config.txt
-
-echo "dtoverlay=i2c-gpio,i2c_gpio_sda=2,i2c_gpio_scl=3,bus=3" >> "/boot/config.txt"
-dpkg -i /home/${FIRST_USER_NAME}/.openplotter/moitessier/moitessier_4.19.75_1.4.0_armhf.deb
 
 systemctl enable pypilot_boatimu
 systemctl enable openplotter-pypilot-read
 systemctl enable openplotter-i2c-read
+
+kernel=$(uname -r)
+kernel0="$(cut -d'-' -f1 <<<"$kernel")"
+wget -r -l1 -np -nd -A .deb -N https://get.rooco.tech/moitessier/buster/release/$kernel0/latest/ || true
+dpkg -i moitessier_$kernel0_*_armhf.deb || true
+rm -f moitessier_$kernel0_*_armhf.deb || true
 EOF
